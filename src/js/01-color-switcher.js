953161e5f21a63942dbb5c074d7123883eadbe55
Hanna <anna_kon@bigmir.net>
2023-01-18 16:07:53 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const bodyEl = document.body;
+let SwitcherTimerId = null;
+
+
+
+startButton.addEventListener('click', onStartBtnClick);
+stopButton.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {    
+SwitcherTimerId = setInterval(changeColor, 1000);
+startButton.setAttribute('disabled', 'true');   
+    
+}
+
+function changeColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+
+function onStopBtnClick() {
+    startButton.removeAttribute('disabled');
+    clearInterval(SwitcherTimerId);
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
 
